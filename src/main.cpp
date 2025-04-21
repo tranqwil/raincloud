1c8c034cd99159b13b3973dcbfa4fc2d696f0f65
tranqwil <will@mistaudio.com>
2025-04-21 14:01:11 +0100

    Fix initialization of Vector3 to prevent build errors in VS

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,8 @@ int main() {
 	InitWindow(800, 600, "RainCloud");
 	SetTargetFPS(60);
 	Camera camera = { 0 };
-	camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // Camera position
-	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };       // Camera looking at point
-	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };           // Camera up vector
+	camera.position = Vector3( 0.0f, 10.0f, 10.0f );  // Camera position	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };       // Camera looking at point
+	camera.up = Vector3( 0.0f, 1.0f, 0.0f );           // Camera up vector
 	camera.fovy = 45.0f;                                 // Camera field-of-view Y
 	camera.projection = CAMERA_PERSPECTIVE;              // Perspective mode
 
